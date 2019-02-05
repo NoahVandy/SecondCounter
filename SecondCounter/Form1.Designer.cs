dd44e0eb2b872205d62b1c19cb2e55cf09190623
Noah Vandervelden <NVandervel@my.gcu.edu>
2019-02-05 13:58:07 -0700

    Added everything

--- SecondCounter/Form1.Designer.cs
+++ SecondCounter/Form1.Designer.cs
@@ -28,13 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtbx_input = new System.Windows.Forms.TextBox();
+            this.lbl_input = new System.Windows.Forms.Label();
+            this.txtbx_output = new System.Windows.Forms.TextBox();
+            this.lbl_output = new System.Windows.Forms.Label();
+            this.btn_convert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtbx_input
+            // 
+            this.txtbx_input.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbx_input.Location = new System.Drawing.Point(513, 172);
+            this.txtbx_input.Name = "txtbx_input";
+            this.txtbx_input.Size = new System.Drawing.Size(254, 60);
+            this.txtbx_input.TabIndex = 0;
+            // 
+            // lbl_input
+            // 
+            this.lbl_input.AutoSize = true;
+            this.lbl_input.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_input.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_input.Location = new System.Drawing.Point(254, 175);
+            this.lbl_input.Name = "lbl_input";
+            this.lbl_input.Size = new System.Drawing.Size(219, 54);
+            this.lbl_input.TabIndex = 1;
+            this.lbl_input.Text = "Seconds:";
+            this.lbl_input.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtbx_output
+            // 
+            this.txtbx_output.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbx_output.Location = new System.Drawing.Point(513, 561);
+            this.txtbx_output.Name = "txtbx_output";
+            this.txtbx_output.Size = new System.Drawing.Size(888, 60);
+            this.txtbx_output.TabIndex = 2;
+            // 
+            // lbl_output
+            // 
+            this.lbl_output.AutoSize = true;
+            this.lbl_output.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_output.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_output.Location = new System.Drawing.Point(32, 564);
+            this.lbl_output.Name = "lbl_output";
+            this.lbl_output.Size = new System.Drawing.Size(441, 54);
+            this.lbl_output.TabIndex = 3;
+            this.lbl_output.Text = "Converted seconds:";
+            this.lbl_output.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btn_convert
+            // 
+            this.btn_convert.Location = new System.Drawing.Point(513, 348);
+            this.btn_convert.Name = "btn_convert";
+            this.btn_convert.Size = new System.Drawing.Size(254, 97);
+            this.btn_convert.TabIndex = 4;
+            this.btn_convert.Text = "Convert";
+            this.btn_convert.UseVisualStyleBackColor = true;
+            this.btn_convert.Click += new System.EventHandler(this.btn_convert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(1710, 858);
+            this.Controls.Add(this.btn_convert);
+            this.Controls.Add(this.lbl_output);
+            this.Controls.Add(this.txtbx_output);
+            this.Controls.Add(this.txtbx_input);
+            this.Controls.Add(this.lbl_input);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtbx_input;
+        private System.Windows.Forms.Label lbl_input;
+        private System.Windows.Forms.TextBox txtbx_output;
+        private System.Windows.Forms.Label lbl_output;
+        private System.Windows.Forms.Button btn_convert;
     }
 }
 
--- SecondCounter/Form1.cs
+++ SecondCounter/Form1.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿//This is my own work
+//Noah Vandervelden
+
+
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,6 +13,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace SecondCounter
 {
     public partial class Form1 : Form
@@ -15,6 +21,42 @@ namespace SecondCounter
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn_convert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //get number of seconds entered and convert it to an int
+                String input = txtbx_input.Text;
+                int seconds = int.Parse(input);
+                //format the seconds entered into days hours and minutes
+                if(seconds >= 86400)
+                {
+                    int days = (seconds / 86400);
+                    int remainingHours = ((seconds % 86400) / 3600);
+                    int remainingMinutes = ((seconds % 86400) % 3600) / 60;
+                    int remainingSeconds = ((seconds % 86400) % 3600) % 60;
+
+                    txtbx_output.Text = days.ToString() + " days " + remainingHours.ToString() + " hours " +  remainingMinutes.ToString() +" minutes " + remainingSeconds.ToString() + " seconds";
+                }
+                else if (seconds >= 3600)
+                {
+                    int hours = (seconds / 3600);
+                    int remainingMinutes = (seconds % 3600) / 60;
+                    txtbx_output.Text = hours.ToString() + " hours " + remainingMinutes.ToString() + " minutes";
+                }
+                else if (seconds >= 60)
+                {
+                    int minutes = (seconds / 60);
+                    int remainingSeconds = (seconds % 60);
+                    txtbx_output.Text = minutes.ToString() + " minutes " + remainingSeconds.ToString() + " seconds"; ;
+                }
+            }
+            catch
+            {
+
+            }
         }
     }
 }
